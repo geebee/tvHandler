f73593fe685ea20234439df1baaafcfeea7b2325
Gee Bee <mike.goldberger@gmail.com>
2010-08-29 21:02:03 -0400

    some changes...?

--- grabTorrents.py
+++ grabTorrents.py
@@ -1,17 +1,30 @@
 import feedparser
-import urllib2
+import urllib, urllib2
 
 storageFolder = "/Users/mike/Downloads/Torrents/"
 
-#South Park Episodes
-feed = feedparser.parse("http://ezrss.it/search/index.php?show_name=South+Park&show_name_exact=false&mode=rss&direct")
-for episode in feed.entries:
-    print("Episode Title: " + episode.title)
-    #fHandle = open(storageFolder + episode.title + ".torrent", "w")
-    #try:
-        #tempTorrent = urllib2.urlopen(episode.link)
-    #except:
-        #print "Couldn't Fetch Torrent: " + episode.title
-    
-    #fHandle.write(tempTorrent.read())
-    #fHandle.close()
+shows = ["South Park", "Futurama"]
+
+for show in shows:
+    encShow = urllib.pathname2url(show)
+    feed = feedparser.parse("http://ezrss.it/search/index.php?show_name=" + encShow + "&show_name_exact=false&mode=rss&direct")
+    print("**************************************************************")
+    print("Show: " + show)
+    print("======================")
+    print("**************************************************************")
+    print("    Title              : " + str(feed.entries[0].title))
+    print("    Updated            : " + str(feed.entries[0].updated))
+    print("    Updated (Parsed)   : " + str(feed.entries[0].updated_parsed))
+    print("    Summary            : " + str(feed.entries[0].summary))
+    print("    Link               : " + str(feed.entries[0].link))
+    print("**************************************************************")
+    #for episode in feed.entries:
+        #print("Episode Title: " + episode.title)
+        #fHandle = open(storageFolder + episode.title + ".torrent", "w")
+        #try:
+            #tempTorrent = urllib2.urlopen(episode.link)
+        #except:
+            #print "Couldn't Fetch Torrent: " + episode.title
+        
+        #fHandle.write(tempTorrent.read())
+        #fHandle.close()
