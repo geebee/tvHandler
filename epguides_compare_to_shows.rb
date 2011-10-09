48e7a71145013f10ac2801391570b7c544930a0b
Michael Goldberger <mike.goldberger@gmail.com>
2011-10-09 16:26:18 -0400

    refactored entire parsing logic to use main page (and be a bit more sane)

--- epguides_compare_to_shows.rb
+++ epguides_compare_to_shows.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
-require 'net/http'
-require 'uri'
-require 'csv'
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'htmlentities'
 
 options = {}
 
@@ -41,61 +42,44 @@ puts "Show Title: #{showTitle}"
 
 epguidesTitle = showTitle.split(" ").join()
 epguidesURL = "http://epguides.com/#{epguidesTitle}/"
-getRageIdPage = Net::HTTP.get(URI.parse(epguidesURL))
-rageId = getRageIdPage.scan(/exportToCSV\.asp\?rage=([0-9]+)/).flatten[0]
 
-puts "Rage ID: #{rageId}"
-
-csvURL = "http://epguides.com/common/exportToCSV.asp?rage=#{rageId}" 
-pageContent = Net::HTTP.get(URI.parse(csvURL))
-
-showCSV = pageContent.gsub!(/<.*?>/, "").gsub!(/^list output\s+$/, "").gsub!(/^\s+$/, "").gsub!(/^$\n/, "")
-
-showStringArray = showCSV.split("\n")
-headerArray = showStringArray.shift.split(",")
+escapeHtmlEntities = HTMLEntities.new
+preShowPage = Nokogiri::HTML(open(epguidesURL)).to_html.gsub!("\n","")
+showPage = escapeHtmlEntities.decode(preShowPage)
 showHashArray = Array.new
-
-showStringArray.each do |episode|
-    # Simple split, doesn't really work if episode titles have commas...
-    #tempEpisodeArray = episode.split(",")
-    # Split regex to not split on commas inside quoted strings
-    tempEpisodeArray = episode.split(/,(?!(?:[^",]|[^"],[^"])+")/)
+showPage.scan(/\d+. +(\d+) *\- *(\d+).*?<a.*?>(.*?)<\/a>/) do |season, episode, title|
+    #puts "Season: #{season}, Episode: #{episode} - Title: #{title}"
     tempEpisodeHash = Hash.new
 
-    headerArray.each_with_index do |column, index|
-        tempEpisodeHash[column] = tempEpisodeArray[index]
-    end
+    tempEpisodeHash[:season] = season
+    tempEpisodeHash[:episode] = episode
+    tempEpisodeHash[:title] = title 
 
-    tempEpisodeHash.delete("production code")
-    tempEpisodeHash["title"].gsub!(/^"(.*?)"$/,'\1')
-
-    tempEpisodeHash["file_name"] = "#{showTitle} - S"
-    if tempEpisodeHash["season"].to_i < 10
-        tempEpisodeHash["file_name"] += "0#{tempEpisodeHash["season"]}"
+    tempEpisodeHash[:file_name] = "#{showTitle} - S"
+    if tempEpisodeHash[:season].to_i < 10
+        tempEpisodeHash[:file_name] += "0#{tempEpisodeHash[:season]}"
     else
-        tempEpisodeHash["file_name"] += "#{tempEpisodeHash["season"]}"
+        tempEpisodeHash[:file_name] += "#{tempEpisodeHash[:season]}"
     end
-    if tempEpisodeHash["episode"].to_i < 10
-        tempEpisodeHash["file_name"] += "E0#{tempEpisodeHash["episode"]}"
-    else
-        tempEpisodeHash["file_name"] += "E#{tempEpisodeHash["episode"]}"
-    end
+    
+    tempEpisodeHash[:file_name] += "E#{tempEpisodeHash[:episode]}"
+    #
     # Put no file extension on the 'file_name' attribute' for matching below
-    tempEpisodeHash["file_name"] += " - #{tempEpisodeHash["title"]}"
+    tempEpisodeHash[:file_name] += " - #{tempEpisodeHash[:title]}"
 
     showHashArray.push(tempEpisodeHash)
 end
 
-#puts "#{tvLocation}/#{showTitle}/Season #{showHashArray[0]["season"]}/#{showHashArray[0]["file_name"]}"
+#puts "#{tvLocation}/#{showTitle}/Season #{showHashArray[0][:season]}/#{showHashArray[0][:file_name]}"
 
 showHashArray.each_with_index do |ep, i|
-    if File.exists?("#{tvLocation}/#{showTitle}/Season #{ep["season"]}/#{ep["file_name"]}.avi")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep["season"]}/#{ep["file_name"]}.mkv")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep["season"]}/#{ep["file_name"]}.mpg")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep["season"]}/#{ep["file_name"]}.mp4")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep["season"]}/#{ep["file_name"]}.m4v")
+    if File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.avi")\
+    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.mkv")\
+    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.mpg")\
+    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.mp4")\
+    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.m4v")
         #puts "Exists"
     else
-        puts "#{tvLocation}/#{showTitle}/Season #{ep["season"]}/#{ep["file_name"]}"
+        puts "#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}"
     end
 end
