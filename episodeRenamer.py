51437a2ce9eec693d61f1796ce7f394caec45c39
Michael Goldberger <mike.goldberger@gmail.com>
2011-10-09 20:08:46 -0400

    added m4v extension to allowable in renaming

--- episodeRenamer.py
+++ episodeRenamer.py
@@ -81,7 +81,7 @@ def parse_filename(show, filename, file_mask):
     return new_filename, info_dictionary
 
 def rename_files(show = None, options = {}):
-    allowableExtensions = [".avi", ".mpg", ".mov", ".mkv", ".mp4"]
+    allowableExtensions = [".avi", ".mpg", ".mov", ".mkv", ".mp4", ".m4v"]
     print("Allowable Extensions for Conversion: " + str(allowableExtensions))
     for filename in os.listdir(options["folder"]):
         extension = os.path.splitext(filename)[1]
