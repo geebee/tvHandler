a72dd9836a97fbcd52a6274748aa3138a392e1e1
Michael Goldberger <mike.goldberger@gmail.com>
2011-10-09 20:09:24 -0400

    massive refactoring; key functionality are now method calls. more still needs to be done

--- epguides_compare_to_shows.rb
+++ epguides_compare_to_shows.rb
@@ -1,10 +1,100 @@
 #!/usr/bin/env ruby
 
+def parseShowToHash(showTitle = "")
+    raise ArgumentError, "showTitle is a required parameter" if showTitle.empty?
+
+    require 'rubygems'
+    require 'nokogiri'
+    require 'open-uri'
+    require 'htmlentities'
+
+    epguidesTitle = showTitle.split(" ").join()
+    epguidesURL = "http://epguides.com/#{epguidesTitle}/"
+
+    escapeHtmlEntities = HTMLEntities.new
+    preShowPage = Nokogiri::HTML(open(epguidesURL)).to_html.gsub!("\n","")
+    showPage = escapeHtmlEntities.decode(preShowPage)
+    showHashArray = Array.new
+
+    #puts "---------"
+    #puts showPage
+    #puts "---------"
+    
+    #showPage.scan(/\d+. +(\d+) *\- *(\d+).*?<a.*?>(.*?)<\/a>/) do |season, episode, title|
+    showPage.scan(/[\d]+[\s]+([\d]+)\-([\d]+).*?([0-9]{2}\/[a-zA-Z]{3}\/[0-9]{2,4}|UNAIRED)[\s]+<a.*?>(.*?)<\/a>/) do |season, episode, airDate, title|
+        #puts "Season: #{season}, Episode: #{episode} - Title: #{title} (Aired: #{airDate})"
+        tempEpisodeHash = Hash.new
+
+        tempEpisodeHash[:season] = season
+        tempEpisodeHash[:episode] = episode
+        tempEpisodeHash[:airDate] = airDate
+        tempEpisodeHash[:title] = title.gsub("\"", "") 
+
+        tempEpisodeHash[:fileName] = "#{showTitle} - S"
+        if tempEpisodeHash[:season].to_i < 10
+            tempEpisodeHash[:fileName] += "0#{tempEpisodeHash[:season]}"
+        else
+            tempEpisodeHash[:fileName] += "#{tempEpisodeHash[:season]}"
+        end
+        
+        tempEpisodeHash[:fileName] += "E#{tempEpisodeHash[:episode]}"
+        #
+        # Put no file extension on the 'fileName' attribute' for matching below
+        tempEpisodeHash[:fileName] += " - #{tempEpisodeHash[:title]}"
+
+        showHashArray.push(tempEpisodeHash)
+    end
+    
+    return showHashArray
+end
+
+
+def compareShowHashToExisting(showHashArray = Hash.new, showTitle = "", tvLocation = "")
+    # Trusty fall-back of a working debug message...
+    # puts "#{tvLocation}/#{showTitle}/Season #{showHashArray[0][:season]}/#{showHashArray[0][:fileName]}"
+
+    raise ArgumentError, "showHashArray is a required parameter" if showHashArray.empty?
+    raise ArgumentError, "showTitle is a required parameter" if showTitle.empty?
+    raise ArgumentError, "tvLocation is a required parameter" if tvLocation.empty?
+
+    require 'date'
+
+    puts "Out of #{showHashArray.length} Episodes in: #{showTitle}..."
+
+    matchingEpisodes = 0
+    missingAndAired = 0
+    unairedEpisodes = 0
+    
+    showHashArray.each_with_index do |ep, i|
+        if File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:fileName]}.avi")\
+        or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:fileName]}.mkv")\
+        or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:fileName]}.mpg")\
+        or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:fileName]}.mp4")\
+        or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:fileName]}.m4v")
+            #puts "Exists"
+            matchingEpisodes += 1
+        else
+            if ep[:airDate] == "UNAIRED"
+                unairedEpisodes += 1
+                break
+            end
+            # If the episode has already aired...
+            if Date.strptime(Time.now.strftime("%d/%b/%y"), "%d/%b/%y") > Date.strptime("#{ep[:airDate]}", "%d/%b/%y")
+                missingAndAired += 1
+                puts ".....Missing Aired Episode: #{ep[:fileName]}"
+            else
+                unairedEpisodes += 1
+                puts ".....Missing Unaired Episode: #{ep[:fileName]}"
+            end
+        end
+    end
+
+    puts "...#{matchingEpisodes} Match as expected."
+    puts "...#{missingAndAired} Have aired, but are not found"
+    puts "...#{unairedEpisodes} Are scheduled, but have not aired."
+end
+
 require 'optparse'
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-require 'htmlentities'
 
 options = {}
 
@@ -40,46 +130,10 @@ end
 puts "Using Directory: #{tvLocation}"
 puts "Show Title: #{showTitle}"
 
-epguidesTitle = showTitle.split(" ").join()
-epguidesURL = "http://epguides.com/#{epguidesTitle}/"
+showsToCheck = ["30 Rock", "Alias", "Arrested Development"]
+# TODO: In future, iterate this array to make the hash of shows to parse, etc...
 
-escapeHtmlEntities = HTMLEntities.new
-preShowPage = Nokogiri::HTML(open(epguidesURL)).to_html.gsub!("\n","")
-showPage = escapeHtmlEntities.decode(preShowPage)
-showHashArray = Array.new
-showPage.scan(/\d+. +(\d+) *\- *(\d+).*?<a.*?>(.*?)<\/a>/) do |season, episode, title|
-    #puts "Season: #{season}, Episode: #{episode} - Title: #{title}"
-    tempEpisodeHash = Hash.new
+shows = Hash.new
 
-    tempEpisodeHash[:season] = season
-    tempEpisodeHash[:episode] = episode
-    tempEpisodeHash[:title] = title 
-
-    tempEpisodeHash[:file_name] = "#{showTitle} - S"
-    if tempEpisodeHash[:season].to_i < 10
-        tempEpisodeHash[:file_name] += "0#{tempEpisodeHash[:season]}"
-    else
-        tempEpisodeHash[:file_name] += "#{tempEpisodeHash[:season]}"
-    end
-    
-    tempEpisodeHash[:file_name] += "E#{tempEpisodeHash[:episode]}"
-    #
-    # Put no file extension on the 'file_name' attribute' for matching below
-    tempEpisodeHash[:file_name] += " - #{tempEpisodeHash[:title]}"
-
-    showHashArray.push(tempEpisodeHash)
-end
-
-#puts "#{tvLocation}/#{showTitle}/Season #{showHashArray[0][:season]}/#{showHashArray[0][:file_name]}"
-
-showHashArray.each_with_index do |ep, i|
-    if File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.avi")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.mkv")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.mpg")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.mp4")\
-    or File.exists?("#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}.m4v")
-        #puts "Exists"
-    else
-        puts "#{tvLocation}/#{showTitle}/Season #{ep[:season]}/#{ep[:file_name]}"
-    end
-end
+shows["#{showTitle}"] = parseShowToHash(showTitle)
+compareShowHashToExisting(shows["#{showTitle}"], showTitle, tvLocation)
