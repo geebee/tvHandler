7aee31c7afc7b33518fac3cb7438408ed69c4f50
Michael Goldberger <mike.goldberger@gmail.com>
2011-10-16 02:58:48 -0400

    added argv command line switches

--- growl/notify.py
+++ growl/notify.py
@@ -16,7 +16,19 @@ if sys.argv[1] == "register":
     p.addNotification()
     s.sendto(p.payload(), addr)
 elif sys.argv[1] == "send":
-    p = GrowlNotificationPacket(title = "Title", description = "Message", priority = 0, sticky = False, password = password)
+    if (len(sys.argv) <= 2 or sys.argv[2] == ""):
+        print("You must pass a message title string as the second argument") 
+        sys.exit(1)
+    else:
+        title = sys.argv[2]
+
+    if (len(sys.argv) <= 3 or sys.argv[3] == ""):
+        print("You must pass a message body string as the third argument") 
+        sys.exit(1)
+    else:
+        description = sys.argv[3]
+
+    p = GrowlNotificationPacket(title = title, description = description, priority = 0, sticky = False, password = password)
     s.sendto(p.payload(), addr)
     s.close()
 else:
